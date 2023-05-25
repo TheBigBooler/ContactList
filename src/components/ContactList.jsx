ae7053a6dfbe93615f02ae24e87964778aadfa11
James <>
2023-05-25 18:05:04 -0500

    working on components

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+function ContactList(){
+    
+}
+
+export default ContactList;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState } from "react";
+import dummyContacts from "../dummyData";
 
 const Main = () => {
+  const [contacts, setContacts] = useState(dummyContacts);
+
+  const list = contacts.map((contact) => {
+    return (
+      <tr key={contact.id}>
+        <td>{contact.name}</td>
+        <td>{contact.phone}</td>
+        <td>{contact.email}</td>
+      </tr>
+    );
+  });
   return (
     <div id="main">
       <div id="navbar">
         <div>Contact List</div>
       </div>
-      <div id="container">{/* your components here */}</div>
+      <div id="container">
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Phone</th>
+              <th>Email</th>
+            </tr>
+            {list}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
